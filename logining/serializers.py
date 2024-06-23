a68b2918b4e434810b621b47db0654f78cc34e5d
toporilka <lepeoshckin.maks@yandex.ru>
2024-06-23 20:35:35 +0300

    some fix for react

--- logining/serializers.py
+++ logining/serializers.py
@@ -4,10 +4,9 @@ from .models import User, Client
 class ClientSerializer(serializers.ModelSerializer):
     class Meta():
         model = Client
-        fields = '__all__'
+        fields = ['name_client','second_name_client','id_user','status']
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta():
         model = User
-        fields = '__all__'
-    
+        fields = ['email_user','password_user','tag_user']
